e43a67c298d801bc1110d1951a582212fb0768c5
Alex <steblyakov.aleksandr@gmail.com>
2026-09-07 19:51:38 +0300

    some sytax fixed

--- src/modules/authorisation/application/usecases/registration.usecase.ts
+++ src/modules/authorisation/application/usecases/registration.usecase.ts
@@ -73,6 +73,7 @@ export class RegisterUserHandler implements ICommandHandler<RegisterUserCommand>
             );
         }
 
+        console.warn("...about to send email... code:", codeToSend );
         // 6. Отправляем подтверждение на почту
         await this.emailService.sendConfirmationEmail(
             newUser.email,
